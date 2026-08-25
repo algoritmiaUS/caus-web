6d73ca9c8d72b806b30bdd3e59be20740c8807f2
Raquezin <fergicu@gmail.com>
2026-08-25 11:31:21 -0400

    add lang-notice

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,12 +50,12 @@ document.addEventListener('keydown', function (e) {
 var langNotice = document.getElementById('lang-notice');
 if (langNotice && !sessionStorage.getItem('lang-notice-shown')) {
   langNotice.classList.remove('hidden');
-  sessionStorage.setItem('lang-notice-shown', '1');
 }
 if (langNotice) {
   document
     .getElementById('lang-notice-close')
     .addEventListener('click', function () {
       langNotice.classList.add('hidden');
+      sessionStorage.setItem('lang-notice-shown', '1');
     });
 }
